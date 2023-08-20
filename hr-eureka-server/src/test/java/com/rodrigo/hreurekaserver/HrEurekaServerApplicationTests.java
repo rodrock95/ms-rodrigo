7eaad781f07230b13f1cc525676e4e0523ba090e
Rodrigo <r.barboza9502@gmail.com>
2023-08-20 11:27:49 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/com/rodrigo/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
