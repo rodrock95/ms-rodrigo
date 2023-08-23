e8aea3295819de4965e61125da295c2e83d3e17d
Rodrigo <r.barboza9502@gmail.com>
2023-08-22 23:05:34 -0300

    Private Git repository

--- hr-worker/src/main/java/com/rodrigo/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/rodrigo/hrworker/resources/WorkerResource.java
@@ -24,8 +24,8 @@ public class WorkerResource {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 	
-	@Value("${test.config}")
-	private String testConfig;
+	//@Value("${test.config}")
+	//private String testConfig;
 	
 	@Autowired
 	private Environment env;
@@ -35,7 +35,7 @@ public class WorkerResource {
 	
 	@GetMapping("/configs")
 	public ResponseEntity<List<Void>> getConfigs(){
-		logger.info("CONFIG = " + testConfig);
+		//logger.info("CONFIG = " + testConfig);
 		return ResponseEntity.noContent().build();
 	}
 	
