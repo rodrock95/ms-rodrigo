7e57e67a0f80667b17db73cf4ae589c0641590cf
Rodrigo <r.barboza9502@gmail.com>
2023-08-26 22:54:32 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/rodrigo/hruser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.rodrigo.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rodrigo.hruser.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	User findByEmail(String email);
+}
