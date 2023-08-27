803b474224c93c8c00fa8d57d504e6f760fcfda4
Rodrigo <r.barboza9502@gmail.com>
2023-08-27 14:49:30 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/rodrigo/hroauth/feignClients/UserFeignClient.java
@@ -0,0 +1,17 @@
+package com.rodrigo.hroauth.feignClients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.rodrigo.hroauth.entities.User;
+
+@Component
+@FeignClient(name = "hr-user", path = "/users")
+public interface UserFeignClient {
+
+	@GetMapping(value = "/search")
+	ResponseEntity<User> findByEmail(@RequestParam String email);
+}
---
+++ hr-oauth/src/main/java/com/rodrigo/hroauth/services/UserService.java
@@ -0,0 +1,22 @@
+package com.rodrigo.hroauth.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodrigo.hroauth.entities.User;
+import com.rodrigo.hroauth.feignClients.UserFeignClient;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		User user = userFeignClient.findByEmail(email).getBody();
+		if(user == null) {
+			throw new IllegalArgumentException("Email not found");
+		}
+		return user;
+	}
+}
