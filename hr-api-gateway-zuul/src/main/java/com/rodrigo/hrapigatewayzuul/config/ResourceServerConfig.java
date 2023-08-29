2a3b4d75f45a825d818b0aa83d459f5e35e20112
Rodrigo <r.barboza9502@gmail.com>
2023-08-28 23:35:41 -0300

    Security configs to config server

--- hr-api-gateway-zuul/src/main/java/com/rodrigo/hrapigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/rodrigo/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -13,15 +13,15 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	
+	@Autowired
+	private JwtTokenStore tokenStore;	
+	
 	private static final String[] PUBLIC = { "/hr-oauth/oauth/token" };
 	
 	private static final String[] OPERATOR = { "/hr-worker/**" };
 	
-	private static final String[] ADMIN = { "/hr-payroll/**", "/hr-user/**" };
+	private static final String[] ADMIN = { "/hr-payroll/**", "/hr-user/**", "/actuator/**",  "/hr-worker/actuator/**", "/hr-oauth/actuator/**"};
 
-	@Autowired
-	private JwtTokenStore tokenStore;
-	
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.tokenStore(tokenStore); 
